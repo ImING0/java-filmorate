042c394cbda84b8852d1adccccae8c3e75379d6d
George Polyansky <83121990+ImING0@users.noreply.github.com>
2023-05-06 02:52:49 +0300

    feat
    добавить переменные в классы

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,6 +2,15 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Film {
+
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private long duration;
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,4 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
+import javax.validation.constraints.Email;
+import java.time.LocalDate;
+
 public class User {
+
+    private Long id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
+
 }
