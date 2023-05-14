088793e82763a45addb107f2ad69aea7bd9e0829
George Polyansky <83121990+ImING0@users.noreply.github.com>
2023-05-14 15:45:43 +0300

    feat
    добавить добавление ,удаление и поиск общих друзей

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -8,6 +8,8 @@ import ru.yandex.practicum.filmorate.validation.LoginCorrect;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Past;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Builder
@@ -22,4 +24,22 @@ public class User {
     private String name;
     @Past
     private LocalDate birthday;
+    @Builder.Default
+    private Set<Long> friends = new HashSet<>();
+
+    public void addFriend(User user) {
+        friends.add(user.getId());
+        user.getFriends().add(this.id);
+    }
+
+    public void removeFriend(User user) {
+        friends.remove(user.getId());
+        user.getFriends().remove(this.id);
+    }
+
+    public Set<Long> findCommonFriendsWithUser(User user) {
+        Set<Long> intersection = new HashSet<>(this.friends);
+        intersection.retainAll(user.getFriends());
+        return intersection;
+    }
 }
